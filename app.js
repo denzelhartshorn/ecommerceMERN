3781f3cd3821c628ef30393a21df86082096b956
Denzel <denzelhartshorn@gmail.com>
2019-09-06 11:23:12 -0700

    express server running

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-require("dotevn").config();
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("hello from node");
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
