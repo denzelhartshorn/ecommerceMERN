32e4cf1bd5d7f327979ffc1f1445fe98092a9f03
Denzel <denzelhartshorn@gmail.com>
2019-09-06 12:42:57 -0700

    added first routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+//import routes
+const userRoutes = require("./routes/user");
 
 //app
 const app = express();
@@ -13,10 +15,8 @@ mongoose
   })
   .then(() => console.log("db connected"));
 
-//routes
-app.get("/", (req, res) => {
-  res.send("hello from node");
-});
+//routes middleware
+app.use(userRoutes);
 
 const port = process.env.PORT || 8001;
 
